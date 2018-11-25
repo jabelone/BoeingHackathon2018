f3317b9b207a9cd5925457b8f48565a90ce6fdbe
jabelone <hello@jaimyn.com.au>
2018-11-25 15:11:04 +1000

    put css/js into file

---
+++ static/main.js
@@ -0,0 +1,37 @@
+let num_planes = 8;
+let plane_entities;
+
+for (let x = 1; x <= num_planes; x++) {
+    plane_entities = document.getElementById("plane" + String(x));
+    M.toast({html: "Plane " + x + " found.", displayLength: 400});
+}
+
+function updateUI() {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState === XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+           if (xmlhttp.status === 200) {
+               let locations = JSON.parse(xmlhttp.responseText);
+               console.log(locations);
+           }
+           else if (xmlhttp.status === 400) {
+              alert('There was an error 400');
+           }
+           else {
+                M.toast({html: 'There was an error fetching data.', displayLength: 1000})
+           }
+        }
+    };
+
+    xmlhttp.open("GET", "/api/locations", true);
+    xmlhttp.send();
+}
+
+window.addEventListener("load", function () {
+    M.toast({html: 'Finished loading situational awareness system.', displayLength: 2000});
+    setTimeout(function () {
+        M.toast({html: 'Loading terrain data...', displayLength: 1000});
+    }, 1000);
+    setInterval(updateUI, 500);
+});
