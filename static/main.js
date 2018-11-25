73a7b88768e3d9e91117bed25f65fccacf1e81e1
jabelone <hello@jaimyn.com.au>
2018-11-25 15:31:03 +1000

    more planes

--- static/main.js
+++ static/main.js
@@ -1,6 +1,5 @@
 let num_planes = 8;
-let plane_entities;
-
+let plane_entities = {};
 
 function updateUI() {
     var xmlhttp = new XMLHttpRequest();
@@ -10,6 +9,16 @@ function updateUI() {
             if (xmlhttp.status === 200) {
                 let locations = JSON.parse(xmlhttp.responseText);
                 console.log(locations);
+
+                for (let x = 1; x <= num_planes; x++) {
+                    lon = locations[x]["location"].split(",")[0];
+                    lat = locations[x]["location"].split(",")[1];
+                    alt = locations[x]["location"].split(",")[2];
+                    console.log(lon);
+                    console.log(lat);
+                    console.log(alt);
+                    // plane_entities[x] = document.getElementById("plane" + String(x));
+                }
             }
             else if (xmlhttp.status === 400) {
                 alert('There was an error 400');
@@ -28,10 +37,10 @@ window.addEventListener("load", function () {
     M.toast({html: 'Finished loading situational awareness system.', displayLength: 2000});
 
     for (let x = 1; x <= num_planes; x++) {
-        // plane_entities = document.getElementById("plane" + String(x));
-        let display_string = "Plane " + x + " found.";
-        M.toast({html: display_string, displayLength: 400});
+        plane_entities[x] = document.getElementById("plane" + String(x));
     }
+
+    M.toast({html: "8 airctaft found.", displayLength: 1000});
 
     setTimeout(function () {
         M.toast({html: 'Loading terrain data...', displayLength: 1000});
